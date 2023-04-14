f531c1d9f3cf6cb4864e4f7378f7ed5bec1308b4
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2023-04-15 00:05:33 +0530

    added

--- server.js
+++ server.js
@@ -7,16 +7,33 @@ var corsOption={
     origin:"https://technotes-gl5i.onrender.com"
 };
 app.use(cors(corsOption));
-db.mongoose.set('strictQuery', true);
-db.mongoose.connect(db.url,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(()=>{
-    console.log('Connected to the database')
-  }).catch((err)=>{
-    console.log(err);
-    process.exit();
-  })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://yuvrajsrivastava6392:<password>@cluster0.wsjfitn.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
   require('./routes/Users.routes')(app);
